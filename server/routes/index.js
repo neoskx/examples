5dd0655edad7b9bc01254d53486522674b6f613e
neo <shaokexu@example.com>
2021-07-26 12:36:43 -0700

    fix issue that in heroku cannot install fs-extra

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const fs = require('fs-extra');
+const express = require("express");
+const { existsSync, readFileSync } = require("fs");
 const path = require("path");
 const router = express.Router();
-const packageJSON = require('../../package.json');
+const packageJSON = require("../../package.json");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const metadataPath = path.join(__dirname, "../../apps-dist/metadata.json");
-  console.log('metadataPath: ', metadataPath);
-  let appsMetadata = [];
-  if(fs.existsSync(metadataPath)){
-    appsMetadata = fs.readJSONSync(metadataPath);
-  }
-  
-  res.render('index', {
-    title: packageJSON.displayName||packageJSON.name||"Examples",
-    description: packageJSON.description,
-    homepage: packageJSON.homepage,
-    keywords: packageJSON.keywords,
-    appsMetadata: appsMetadata
-  });
+router.get("/", function (req, res, next) {
+    const metadataPath = path.join(__dirname, "../../apps-dist/metadata.json");
+    console.log("metadataPath: ", metadataPath);
+    let appsMetadata = [];
+    if (existsSync(metadataPath)) {
+        appsMetadata = JSON.parse(readFileSync(metadataPath, "utf8"));
+    }
+
+    res.render("index", {
+        title: packageJSON.displayName || packageJSON.name || "Examples",
+        description: packageJSON.description,
+        homepage: packageJSON.homepage,
+        keywords: packageJSON.keywords,
+        appsMetadata: appsMetadata,
+    });
 });
 
 module.exports = router;
