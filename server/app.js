43b3143167085e5c8e28470f0356b7a3cad054d7
neo <shaokexu@example.com>
2021-07-26 20:19:16 -0700

    add github action

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../apps-dist')));
 
-build(app);
+// build(app);
 
 app.use('/', indexRouter);
 
